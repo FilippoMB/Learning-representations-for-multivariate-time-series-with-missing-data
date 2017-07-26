01a2d23b4375c39a531fe159353f34577cd333d4
Filippo Bianchi <filippo.m.bianchi@uit.no>
2017-07-26 19:24:43 +0200

    Inserted alternate train (inf and ext)
    Training can be done alternating the error computed on ground truth inputs (ext) and the ones inferred by the output

--- gen_seq2seq_TF.py
+++ gen_seq2seq_TF.py
@@ -14,7 +14,6 @@ class s2sModel():
         self.max_gradient_norm = config['max_gradient_norm']
         self.learning_rate = config['learning_rate']
         self.EOS = 0
-        self.PAD = 0
         
         self._make_graph()
         
@@ -31,8 +30,7 @@ class s2sModel():
             self._init_bidirectional_encoder()
             
         # ----- DECODER -----
-#        self._init_decoder()
-        self._init_decoder_TF()
+        self._init_decoder()
         
         # ----- LOSS -----
         self._init_loss()
@@ -102,81 +100,7 @@ class s2sModel():
                 self.encoder_state = tuple(tf.concat((encoder_fw_state[i], encoder_bw_state[i]), 1) for i in range(self.num_layers))
     
     
-    # Native implementation with training on inferenced outputs            
-    def _init_decoder(self):      
-        with tf.variable_scope("Decoder"):    
-            decoder_lengths = self.encoder_inputs_length +  1 # extra length for the leading <EOS> token in decoder inputs
-                    
-            # outputs weights. need to be specified explicitly (no dense layer) as they are used multiple times 
-            # TODO: try also glorot_normal
-            W = tf.get_variable("W_dec", shape=[self.decoder_units, self.input_dim], initializer=tf.contrib.keras.initializers.glorot_uniform(), dtype=tf.float32)
-            b = tf.get_variable("b_dec", shape=[self.input_dim], initializer=tf.zeros_initializer(), dtype=tf.float32)
-            
-            # initial decoder input and pad slice (not used)
-            _, d_batch_size, _ = tf.unstack(tf.shape(self.encoder_inputs))
-            self.EOS_slice = tf.ones([1,d_batch_size,self.input_dim], dtype=tf.float32, name='EOS_slice')*self.EOS
-            PAD_time_slice = tf.ones([d_batch_size, self.input_dim], dtype=tf.float32, name='PAD_slice')*self.PAD
-                        
-            # loop function (initial step)
-            def loop_fn_initial():
-                initial_elements_finished = (0 >= decoder_lengths)  # all False at the initial step
-                initial_input = self.EOS_slice[0,:,:]
-                initial_cell_state = self.encoder_state
-                initial_cell_output = None
-                initial_loop_state = None  # we don't need to pass any additional information -> loop_state ALWAYS None
-                return (initial_elements_finished,
-                        initial_input,
-                        initial_cell_state,
-                        initial_cell_output,
-                        initial_loop_state)
-            
-            # loop function (transition step)
-            def loop_fn_transition(time, previous_output, previous_state, previous_loop_state):
-            
-                def get_next_input():
-                    next_input = tf.add(tf.matmul(previous_output, W), b) # previous output of the DECODER
-                    return next_input
-                
-                elements_finished = (time >= decoder_lengths) # this operation produces boolean tensor of [batch_size,]
-                                                              # defining if corresponding sequence has ended
-            
-                finished = tf.reduce_all(elements_finished) # -> boolean scalar (logical AND between the elements of the input tensor)
-                
-                # the next decoder input is 0 (PAD) if the maximum number of decoder outputs have been produced
-                _next_input = tf.cond(finished, lambda: PAD_time_slice, get_next_input) 
-                
-                state = previous_state # state is just passed through
-                output = previous_output
-                loop_state = None # loop state is just passed through
-            
-                return (elements_finished, 
-                        _next_input,
-                        state,
-                        output,
-                        loop_state)
-            
-            # combine the 2 loop function    
-            def loop_fn(time, previous_output, previous_state, previous_loop_state):
-                if previous_state is None:    # time == 0
-                    assert previous_output is None and previous_state is None # why this assert??
-                    return loop_fn_initial()
-                else:
-                    return loop_fn_transition(time, previous_output, previous_state, previous_loop_state)
-            
-            decoder_outputs_ta, _, _ = tf.nn.raw_rnn(self.decoder_cell, loop_fn)
-            decoder_outputs = decoder_outputs_ta.stack() # outputs are a list of 2D tensors. Stack them in a single 3D tensor to compute the output projections
-            
-            # flatten the outputs since tf.matmul() can multiply only 2D tensors
-            decoder_max_steps, decoder_batch_size, decoder_dim = tf.unstack(tf.shape(decoder_outputs))
-            decoder_outputs_flat = tf.reshape(decoder_outputs, (-1, decoder_dim))
-            decoder_pred_flat = tf.add(tf.matmul(decoder_outputs_flat, W), b) # here is where we use W and b again
-            
-            # restore the shape [time_steps, batch_size, input_dim]
-            self.tr_outputs = tf.reshape(decoder_pred_flat, (decoder_max_steps, decoder_batch_size, self.input_dim))
-            
-    
-    # implementation using TF seq2seq library          
-    def _init_decoder_TF(self):
+    def _init_decoder(self):
         with tf.variable_scope("Decoder") as decoder_scope: 
             
             _, d_batch_size, _ = tf.unstack(tf.shape(self.encoder_inputs)) 
@@ -227,10 +151,6 @@ class s2sModel():
             # callable that takes (time, outputs, state, sample_ids) and emits (finished, next_inputs, next_state)
             def next_inputs_fn(time, outputs, state, sample_ids): 
                 del sample_ids
-#                finished = tf.tile([False], [d_batch_size])
-#                next_inputs = outputs
-#                max_time = tf.ones([d_batch_size], dtype=tf.int32) * 10
-#                finished = (time >= max_time)
                 finished = (time >= decoder_train_lengths)
                 next_inputs = tf.where(finished, self.EOS_slice[0,:,:], outputs)
                 return (finished, next_inputs, state)
@@ -250,19 +170,22 @@ class s2sModel():
                     output_layer=self.output_layer)  # projection applied per timestep
                     
             # Dynamic decoding
-            inf_outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
+            inf_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
                     inf_decoder,
                     maximum_iterations=tf.reduce_max(decoder_train_lengths),
                     output_time_major=True,
                     swap_memory=True,
                     scope=decoder_scope)
             
-            #self.inf_outputs = self.output_layer(inf_outputs.rnn_output)
             self.inf_outputs = inf_outputs.rnn_output
             
             
     def _init_loss(self):
         with tf.variable_scope("Loss"): 
+            
+            decoder_train_outputs = tf.concat([self.decoder_outputs, self.EOS_slice], axis=0)
+            parameters = tf.trainable_variables()
+            optimizer = tf.train.AdamOptimizer(self.learning_rate)
             
             # TODO: L2 norm and lr decay
             # L2 regularization (to avoid overfitting and to have a better generalization capacity)
@@ -272,22 +195,27 @@ class s2sModel():
 #                if not ("Bias" in tf_var.name or "Output_" in tf_var.name):
 #                    reg_loss += tf.reduce_mean(tf.nn.l2_loss(tf_var))
             
-            # compute training loss
-            decoder_train_outputs = tf.concat([self.decoder_outputs, self.EOS_slice], axis=0)
-#            self.train_loss = tf.losses.mean_squared_error(labels=decoder_train_outputs, predictions=self.tr_outputs)
-            self.train_loss = tf.losses.mean_squared_error(labels=decoder_train_outputs, predictions=self.inf_outputs) # when training with TF inference
+            # --------- TRAINING LOSS ---------
+            self.tr_loss = tf.losses.mean_squared_error(labels=decoder_train_outputs, predictions=self.tr_outputs)
             
             # Calculate and clip gradients
-            parameters = tf.trainable_variables()
-            gradients = tf.gradients(self.train_loss, parameters)
-            clipped_gradients, _ = tf.clip_by_global_norm(gradients, self.max_gradient_norm)
+            tr_gradients = tf.gradients(self.tr_loss, parameters)
+            tr_clipped_gradients, _ = tf.clip_by_global_norm(tr_gradients, self.max_gradient_norm)
             
-            # Optimization
-            optimizer = tf.train.AdamOptimizer(self.learning_rate)
-            self.update_step = optimizer.apply_gradients(zip(clipped_gradients, parameters))
+            self.tr_update_step = optimizer.apply_gradients(zip(tr_clipped_gradients, parameters))
+            
+            # --------- INFERENCE LOSS ---------
+            self.inf_loss = tf.losses.mean_squared_error(labels=decoder_train_outputs, predictions=self.inf_outputs)
+            
+            # Calculate and clip gradients
+            inf_gradients = tf.gradients(self.inf_loss, parameters)
+            inf_clipped_gradients, _ = tf.clip_by_global_norm(inf_gradients, self.max_gradient_norm)
+                       
+            self.inf_update_step = optimizer.apply_gradients(zip(inf_clipped_gradients, parameters))
             
             # tensorboard
-            tf.summary.scalar('train_loss', self.train_loss)
+            tf.summary.scalar('tr_loss', self.tr_loss)
+            tf.summary.scalar('inf_loss', self.inf_loss)
             tvars = tf.trainable_variables()
             for tvar in tvars:
                 tf.summary.histogram(tvar.name.replace(':','_'), tvar)
